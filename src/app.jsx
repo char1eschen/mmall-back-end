6f7863930f84a23704af5da5f3234634f067b33a
char1eschen <martian2005@gmail.com>
2018-07-11 15:56:00 +1000

    update webpack config

---
+++ src/app.jsx
@@ -0,0 +1,20 @@
+/*
+* @Author: char1eschen
+* @Date:   2018-07-10 23:16:20
+* @Last Modified by:   char1eschen
+* @Last Modified time: 2018-07-10 23:48:33
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import 'font-awesome/css/font-awesome.css';
+import './index.css';
+import './index.scss';
+
+ReactDOM.render(
+  <div>
+  <h1>Hello, world</h1>
+  </div>,
+  document.getElementById('app')
+);
